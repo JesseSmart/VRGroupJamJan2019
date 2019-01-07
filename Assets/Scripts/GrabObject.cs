2be8cd181824672f7fe61b81c0ebdeb5ab7cc8f9
JesseSmart <jesse.oscar.smart@gmail.com>
2019-01-07 19:29:20 +1100

    Added grab and hold mechanics and toggle grab mechanic

---
+++ Assets/Scripts/GrabObject.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrabObject : MonoBehaviour
+{
+    protected Rigidbody rigidBody;
+    protected bool originalKinematicState;
+    protected Transform originalParent;
+
+    private Hand cont;
+
+    Vector3 velo = Vector3.zero;
+    Vector3 lastPos = Vector3.zero;
+
+    public bool canGrab = true;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if(cont != null)
+        {
+            velo = (cont.transform.position - lastPos) / Time.deltaTime;
+            lastPos = cont.transform.position;
+        }
+    }
+
+    void Awake()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+        originalKinematicState = rigidBody.isKinematic;
+        originalParent = transform.parent;
+    }
+
+    public void Pickup(Hand controller)
+    {
+        if (!canGrab)
+        {
+            return;
+        }
+        print("Grab");
+
+        controller.holdingItem = true;
+        controller.heldItem = gameObject;
+
+        rigidBody.isKinematic = true;
+
+        canGrab = false;
+
+        transform.SetParent(controller.grabPoint.transform);
+        transform.localPosition = Vector3.zero;
+        transform.localRotation = Quaternion.identity;
+
+        cont = controller;
+    }
+
+    public void Release(Hand controller)
+    {
+        if (transform.parent == controller.grabPoint.gameObject.transform)
+        {
+            print("Release");
+            rigidBody.isKinematic = originalKinematicState;
+
+            if (originalParent != controller.grabPoint.gameObject.transform)
+            {
+                transform.SetParent(originalParent);
+            }
+            else
+            {
+                transform.SetParent(null);
+            }
+
+            if (cont != null)
+            {
+                rigidBody.velocity = velo * 1;
+            }
+
+            lastPos = Vector3.zero;
+            velo = Vector3.zero;
+
+            cont.timeheld = 0;
+            cont.holdingItem = false;
+            cont.heldItem = null;
+            cont = null;
+
+            canGrab = true;
+            
+        }
+    }
+}
+
---
+++ Assets/Scripts/Hand.cs
@@ -0,0 +1,148 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class Hand : MonoBehaviour
+{
+    public GameObject grabPoint;
+
+    public enum SelectedHand
+    {
+        Left,
+        Right
+    }
+
+    public SelectedHand selected;
+
+    public bool holdingItem = false;
+    public GameObject heldItem;
+    float cd;
+    public float timeheld;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (holdingItem)
+            timeheld += Time.deltaTime;
+        cd -= Time.deltaTime;
+
+        if (selected == SelectedHand.Left && SteamVR_Input._default.inActions.GrabPinch.GetStateDown(SteamVR_Input_Sources.LeftHand))
+        {
+            if (holdingItem)
+            {
+                if (heldItem != null && timeheld > 0.5f)
+                {
+                    if (heldItem.CompareTag("Tool"))
+                    {
+                        heldItem.GetComponent<GrabObject>().Release(this);
+                        cd = .5f;
+                        print("toggle drop");
+                    }
+                }
+            }
+        }
+
+        if (selected == SelectedHand.Right && SteamVR_Input._default.inActions.GrabPinch.GetStateDown(SteamVR_Input_Sources.RightHand))
+        {
+            if (holdingItem)
+            {
+                if (heldItem != null && timeheld > 0.5f)
+                {
+                    if (heldItem.CompareTag("Tool"))
+                    {
+                        heldItem.GetComponent<GrabObject>().Release(this);
+                        cd = .5f;
+                        print("toggle drop");
+                    }
+                }
+            }
+        }
+
+        /*if (SteamVR_Input._default.inActions.Teleport.GetStateDown(SteamVR_Input_Sources.Any))
+        {
+            print("Track pad clicked");
+        }
+
+        if (SteamVR_Input._default.inActions.GrabGrip.GetStateDown(SteamVR_Input_Sources.Any))
+        {
+            print("grab grip clicked");
+        }
+
+        float triggerValue = SteamVR_Input._default.inActions.Squeeze.GetAxis(SteamVR_Input_Sources.Any);
+        if (triggerValue > 0.25f)
+        {
+            print("Trigger was pulled: " + triggerValue);
+        }
+
+        if (SteamVR_Input._default.inActions.MenuClick.GetStateDown(SteamVR_Input_Sources.Any))
+        {
+            print("menu clicked");
+        }
+
+        if (SteamVR_Input._default.inActions.GrabPinch.GetStateDown(SteamVR_Input_Sources.Any))
+        {
+            print("Trigger used as button");
+        }
+
+        Vector2 touchPadPos = SteamVR_Input._default.inActions.trackPadPosition.GetAxis(SteamVR_Input_Sources.Any);
+        if (touchPadPos.magnitude > 0.0f)
+        {
+            print("Touchpad Position" + touchPadPos);
+        }*/
+    }
+
+    void OnTriggerStay(Collider collide)
+    {
+        if (collide.gameObject.GetComponent<GrabObject>())
+        {
+            if (selected == SelectedHand.Left)
+            {
+                float triggerValue = SteamVR_Input._default.inActions.Squeeze.GetAxis(SteamVR_Input_Sources.LeftHand);
+                if (triggerValue > 0.25f)
+                {
+                    if (!holdingItem && cd <= 0)
+                    {
+                        collide.GetComponent<GrabObject>().Pickup(this);
+                    }
+                    return;
+                }
+
+                if (triggerValue < 0.25f)
+                {
+                    if (!collide.CompareTag("Tool"))
+                    {
+                        collide.GetComponent<GrabObject>().Release(this);
+                    }
+                    return;
+                }
+            }
+
+            if (selected == SelectedHand.Right)
+            {
+                float triggerValue = SteamVR_Input._default.inActions.Squeeze.GetAxis(SteamVR_Input_Sources.RightHand);
+                if (triggerValue > 0.25f)
+                {
+                    if (!holdingItem && cd <= 0)
+                    {
+                        collide.GetComponent<GrabObject>().Pickup(this);
+                    }
+                    return;
+                }
+
+                if (triggerValue < 0.25f)
+                {
+                    if (!collide.CompareTag("Tool"))
+                    {
+                        collide.GetComponent<GrabObject>().Release(this);
+                    }
+                    return;
+                }
+            }
+        }
+    }
+}
